9540630a2aa8e29582394f7615d337f809346629
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-05-26 00:03:52 +0900

    LoginControllerのIndexメソッドを作ってみた( ﾟДﾟ)

--- EmployeeManagementApp/Controllers/LoginController.cs
+++ EmployeeManagementApp/Controllers/LoginController.cs
@@ -4,17 +4,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace EmployeeManagementApp.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
         DatabaseEntities db = new DatabaseEntities();
+        readonly LoginMembershipProvider loginMembership = new LoginMembershipProvider();
 
         // GET: Login
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "username, password")] loginuser model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (loginMembership.ValidateUser(username:model.name, password:model.password))
+                {
+                    FormsAuthentication.SetAuthCookie(userName:model.name, createPersistentCookie:false);
+                    return RedirectToAction(actionName: "Index", controllerName: "Employee");
+                }
+            }
+
+            ViewBag.Message = "ログインに失敗しました。";
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction(actionName:"Index");
         }
 
         public ActionResult List()
