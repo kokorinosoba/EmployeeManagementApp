8c89404d542deb6646e6f31423849e7745fce2c7
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-05-26 14:51:05 +0900

    ログイン機能を追加 (#3)
    * LoginMembershipProviderを追加
    
    * LoginControllerのIndexメソッドを作ってみた( ﾟДﾟ)
    
    * ログインのびゅーを追加した(゜-゜)
    
    * (=ﾟωﾟ)ﾉLoginuserモデルのほうをいじくりまわしてしまったっぽい
    
    * ログアウトのリンク作った。動作確認はしていない(;^ω^)
    
    * Revert "LoginMembershipProviderを追加"
    
    This reverts commit eaea5a3792694628cef0a6a61e5aad892dc97b81.
    
    * UserControllerとその仲間たちを追加した(*´з`)
    
    * ログイン機能を実装した( ..)φ

--- EmployeeManagementApp/Controllers/LoginController.cs
+++ EmployeeManagementApp/Controllers/LoginController.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace EmployeeManagementApp.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
         DatabaseEntities db = new DatabaseEntities();
@@ -15,6 +17,29 @@ namespace EmployeeManagementApp.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "name, password")] loginuser model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.name == "admin" && model.password == "pass")
+                {
+                    FormsAuthentication.SetAuthCookie(userName:model.name, createPersistentCookie:false);
+                    return RedirectToAction(actionName: "Index", controllerName: "User");
+                }
+            }
+
+            ViewBag.Message = "ログインに失敗しました。";
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction(actionName:"Index");
         }
 
         public ActionResult List()
